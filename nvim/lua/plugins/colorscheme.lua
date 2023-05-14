83830bc88472738420f3b301a5027a6b95a57754
stiffjobs <charleschen0306@gmail.com>
2023-05-14 20:12:55 +0800

    disable some stuff

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -10,26 +10,44 @@ return {
   {
     "rose-pine/neovim",
     as = "rose-pine",
+    -- config = function()
+    --   vim.cmd.colorscheme("rose-pine")
+
+    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    -- end,
   },
-  -- {
-  --   "Mofiqul/vscode.nvim",
-  -- },
+  {
+    "Mofiqul/vscode.nvim",
+  },
   {
     "catppuccin/nvim",
     name = "catppuccin",
   },
-  { "EdenEast/nightfox.nvim" },
-
-  -- Configure LazyVim to load gruvbox
   {
-    "LazyVim/LazyVim",
+    "EdenEast/nightfox.nvim",
+  },
+  {
+    "folke/tokyonight.nvim",
     opts = {
-      colorscheme = "terafox",
-      -- colorscheme = "gruvbox",
-      -- colorscheme = "vscode",
-      -- colorscheme = "rose-pine-moon",
-      -- colorscheme = "catppuccin-macchiato",
-      -- colorscheme = "tokyonight",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
     },
   },
+
+  -- Configure LazyVim to load gruvbox
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "nightfox",
+  -- colorscheme = "gruvbox",
+  -- colorscheme = "vscode",
+  -- colorscheme = "rose-pine-moon",
+  -- colorscheme = "catppuccin-macchiato",
+  -- colorscheme = "tokyonight",
+  --   },
+  -- },
 }
