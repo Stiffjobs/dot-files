1a866af2008c1fc3a944e01fc487ec7b65e48b83
stiffjobs <charleschen0306@gmail.com>
2023-09-22 17:59:58 +0800

    now using symbolic link

---
+++ nvim/init.lua
@@ -0,0 +1,8 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+ColorMyPencils("rose-pine")
+-- ColorMyPencils("vscode")
+-- vim.cmd.colorscheme("gruvbox")
+-- vim.cmd.colorscheme("rose-pine")
+-- vim.cmd.colorscheme("catppuccin")
+-- vim.cmd.colorscheme("tokyonight")
