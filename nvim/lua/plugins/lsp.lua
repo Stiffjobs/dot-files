845dbb26bb996c5ab654b83e5dd87a8e5df71fa2
stiffjobs <charleschen0306@gmail.com>
2023-12-29 13:59:19 +0800

    add some pyright configurations

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -62,6 +62,19 @@ return {
             },
           },
         },
+        pyright = {
+          settings = {
+            pyright = { autoImportCompletion = true },
+            python = {
+              analysis = {
+                autoSearchPaths = true,
+                diagnosticMode = "openFilesOnly",
+                useLibraryCodeForTypes = true,
+                typeCheckingMode = "off",
+              },
+            },
+          },
+        },
       },
       -- you can do any additional lsp server setup here
       -- return true if you don't want this server to be setup with lspconfig
